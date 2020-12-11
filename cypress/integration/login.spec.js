07d68dbeb7131286ce35f73cbfbabf75e3125668
Kath <triggskatherine@gmail.com>
2020-12-11 16:29:35 +1300

    Creates login tests

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,5 +1,15 @@
-// login
-
 describe('login', () => {
-    cy.visit('https://xero.com/nz')
-})
+    it('visits the homepage', () => {
+        cy.visit('https://www.xero.com/nz');
+    });
+    it('clicks the login button', () => {
+        cy.contains('Log in').click();
+    });
+    it('displays the login page', () => {
+        cy.get('h2').should('contain', 'Log in to Xero');
+    });
+    it('displays the username and password fields with placeholder text only', () => {
+        cy.get('#xl-form-email').should('exist').and('have.attr','placeholder','Email address');
+        cy.get('#xl-form-password').should('exist').and('have.attr','placeholder','Password' )
+    });
+});
